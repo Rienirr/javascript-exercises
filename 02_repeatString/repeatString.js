3f393101fdf95cfaa149ea7c2f631a8fbf3c2b86
Rienirr <90462305+Rienirr@users.noreply.github.com>
2022-03-18 09:21:02 +0100

    Prueba cons test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    let stringRepeated="";
+    for(let i=0; i<times;i++){
+stringRepeated+= string; 
+    }
+    if (times<0) return "ERROR";
+    return stringRepeated;
 };
 
 // Do not edit below this line
